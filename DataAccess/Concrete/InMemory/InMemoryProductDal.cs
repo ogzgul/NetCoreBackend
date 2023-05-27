9c95364a57bf50adff0010dff205b4ed6965b6bc
Oguzzz <oguz.gul216@gmail.com>
2023-05-27 12:40:30 +0300

    Authorization

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -16,11 +16,11 @@ namespace DataAccess.Concrete.InMemory
         public InMemoryProductDal()
         {
             _products= new List<Product> {
-            new Product{ ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitInStock=15},
-            new Product{ ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitInStock=3},
-            new Product{ ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitInStock=2},
-            new Product{ ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitInStock=65},
-            new Product{ ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitInStock=1}};
+            new Product{ ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15},
+            new Product{ ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitsInStock=3},
+            new Product{ ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2},
+            new Product{ ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65},
+            new Product{ ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1}};
         }
         public void Add(Product product)
         {
@@ -65,7 +65,7 @@ namespace DataAccess.Concrete.InMemory
             productToUpdate.ProductName = product.ProductName;
             productToUpdate.CategoryId = product.CategoryId;
             productToUpdate.UnitPrice = product.UnitPrice;
-            productToUpdate.UnitInStock = product.UnitInStock;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
         }
     }
 }
